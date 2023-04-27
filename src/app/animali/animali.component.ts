a58c1b1aecf5c009076d6d3fbf31a203e7b64784
MemikKaratas46 <karatas.memik@iisgalvanimi.edu.it>
2023-04-27 13:00:01 +0000

    finito

---
+++ src/app/animali/animali.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Animale } from './animali.model';
+@Component({
+  selector: 'app-animali',
+  templateUrl: './animali.component.html',
+  styleUrls: ['./animali.component.css']
+})
+export class AnimaliComponent {
+  @Input() animale: Animale;
+  
+  constructor(){
+
+  }
+
+  ngOnInit(){}
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Animale } from './animali/animali.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '01_Angular_empty';
+  animali:Animale[] = [
+    {
+      "nome": "cane",
+      "verso": "bau",
+      "zampe": 4,
+      "movimento": "cammina"
+   },
+   {
+      "nome": "rana",
+      "verso": "cra",
+      "zampe": 4,
+      "movimento": "salta"
+   },
+   {
+      "nome": "canarino",
+      "verso": "fiu",
+      "zampe": 2,
+      "movimento": "vola"
+   },
+   {
+      "nome": "serpente",
+      "verso": "fss",
+      "zampe": 0,
+      "movimento": "striscia"
+   }
+  ];
+  OrdinaAnimaliNome(){
+    return this.animali.sort((a: Animale, b: Animale) => a.nome.localeCompare(b.nome));
+  }
+  OrdinaAnimaliZampe(){
+    return this.animali.sort((a: Animale, b: Animale) => a.zampe - b.zampe);
+  }
+  constructor(){
+  }
 }
